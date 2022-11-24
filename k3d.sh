48121f9d9b4694a9638439745cb730ee49cc1dab
Oleksandr Simonov <oleksandr@amoniac.eu>
2022-11-24 22:01:12 +0200

    updates

--- k3d.sh
+++ k3d.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+K3S_VERSION="1.24.8"
+CERT_MANAGER="1.10.1"
+NGINX_INGRESS="4.4.0"
+
 if ![ -x "$(command -v k3d)" > /dev/null 2>&1 ]; then
   echo "The k3d binary is not available or not in your \$PATH"
   exit 1
@@ -48,7 +52,7 @@ spec:
   chart: cert-manager
   repo: https://charts.jetstack.io
   targetNamespace: cert-manager
-  version: v1.8.2
+  version: v${CERT_MANAGER}
   set:
     installCRDs: "true"
 EOF
@@ -63,7 +67,7 @@ spec:
   chart: ingress-nginx
   repo: https://kubernetes.github.io/ingress-nginx
   targetNamespace: ingress-nginx
-  version: 4.1.4
+  version: ${NGINX_INGRESS}
   set:
    controller.publishService.enabled: "true"
 EOF
@@ -75,15 +79,17 @@ EOF
 #      - http://${registry_domain}:${registry_port}
 #EOF
 
-echo "Create a local registry"
-k3d registry create k3s.localhost --port 0.0.0.0:32000 || exit 1
 echo "Create a cluster"
 k3d cluster create k3s -p "80:80@loadbalancer" -p "443:443@loadbalancer" --wait \
   --volume "${manifests}:/var/lib/rancher/k3s/server/manifests@server:0" \
   --volume "${manifests}:/var/lib/rancher/k3s/server/manifests@agent:*" \
-  --registry-use k3d-k3s.localhost:32000 \
   --k3s-arg '--no-deploy=traefik@server:0' \
   --k3s-arg '--no-deploy=traefik@agent:*' \
-  --image rancher/k3s:v1.23.7-k3s1 || exit 1
+  --image rancher/k3s:v${K3S_VERSION}-k3s1 || exit 1
 echo "Merge k3s config"
 k3d kubeconfig merge k3s --kubeconfig-switch-context || exit 1
+echo "DO NOT FORGET TO EDIT coreedns with docker.test host"
+echo "command for this: kubectl -n kube-system edit configmap coredns"
+echo "After you need to kill coredns pod or via kubectl or via k9s"
+echo "!!!IMPORTANT!!!"
+echo "docker.test must have same IP as host.k3d.internal"
